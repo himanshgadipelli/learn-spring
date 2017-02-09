6641c7666ec0a1183ecae4814f2e33c35d2b4b75
bobby <himanshgadipelli@gmail.com>
2017-02-09 09:40:02 -0500

    Commit for verification

--- injection/src/main/java/com/apress/BookwormOracle.java
+++ injection/src/main/java/com/apress/BookwormOracle.java
@@ -12,6 +12,6 @@ public class BookwormOracle implements Oracle{
 
     @Override
     public String defineMeaningOfLife(){
-        return "Encyclopedias are a waste of money - use the Internet";
+        return "Encyclopedias are a waste of money - use the Internet!";
     }
 }
